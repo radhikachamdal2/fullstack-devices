c7285250c2c7e7fa5b0500363bf91551493640f6
RadhikaChamdal <62136253+RadhikaChamdal@users.noreply.github.com>
2025-02-15 17:07:37 +0000

    installed all packages and started creating services

---
+++ services/account-service/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer } from "apollo-server";
+import { buildSubgraphSchema } from "@apollo/subgraph";
+import gql from "graphql-tag";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const typeDefs = gql`
+  extend type Query {
+    accounts: [Account]
+  }
+
+  type Account @key(fields: "id") {
+    id: ID!
+    name: String!
+    email: String!
+    devices: [Device] # To be resolved from Devices Service
+  }
+`;
+
+const accounts = [
+  { id: "1", name: "Test", email: "test@example.com" },
+  { id: "2", name: "Radhika", email: "radhika@example.com" }
+];
+
+const resolvers = {
+  Query: {
+    accounts: () => accounts,
+  }
+};
+
+const server = new ApolloServer({ schema: buildSubgraphSchema({ typeDefs, resolvers }) });
+
+server.listen({ port: 4001 }).then(({ url }) => {
+  console.log(`🚀 Account Service running at ${url}`);
+});
